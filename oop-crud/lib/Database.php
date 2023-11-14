b2c8443bdcd40f3fa7672aa7bd04581dfdd58d7a
Foysal Imran <fimran2011@gmail.com>
2023-11-14 10:42:13 +0600

    update

---
+++ oop-crud/lib/Database.php
@@ -0,0 +1,11 @@
+<?php
+    include_once '../config/config.php';
+
+    class Database{
+        public $host = HOST;
+        public $user = USER;
+        public $password = PASSWORD;
+        public $database = DATABASE;
+    }
+
+?>
